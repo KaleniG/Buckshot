eb7e179ab3adf2fd9c7b5cf52e59bc3451aeba52
KaleniG <kalenig.01@gmail.com>
2024-01-29 22:03:35 +0100

    Window Events

--- Buckshot/src/Buckshot/Application.h
+++ Buckshot/src/Buckshot/Application.h
@@ -3,6 +3,7 @@
 #include "Core.h"
 #include "Window.h"
 #include "Events/Event.h"
+#include "Events/ApplicationEvent.h"
 
 namespace Buckshot {
 
@@ -13,7 +14,11 @@ namespace Buckshot {
     virtual ~Application();
     void Run();
 
+    void OnEvent(Event& e);
+
   private:
+    bool OnWindowClose(WindowCloseEvent& e);
+
     std::unique_ptr<Window> m_Window;
     bool m_Running = true;
   };
