7a4bd92c0d01ea7edcc6ecc51efa00c0760aec84
KaleniG <kalenig.01@gmail.com>
2024-02-23 22:13:22 +0100

    Added VertexBuffer Layout utility

--- Buckshot/src/Buckshot/Application.cpp
+++ Buckshot/src/Buckshot/Application.cpp
@@ -12,6 +12,28 @@ namespace Buckshot {
 
   Application* Application::s_Instance = nullptr;
 
+  static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+  {
+    switch (type)
+    {
+    case ShaderDataType::None:    break;
+    case ShaderDataType::Bool:    return GL_BOOL;
+    case ShaderDataType::Float:   return GL_FLOAT;
+    case ShaderDataType::Float2:  return GL_FLOAT;
+    case ShaderDataType::Float3:  return GL_FLOAT;
+    case ShaderDataType::Float4:  return GL_FLOAT;
+    case ShaderDataType::Int:     return GL_INT;
+    case ShaderDataType::Int2:    return GL_INT;
+    case ShaderDataType::Int3:    return GL_INT;
+    case ShaderDataType::Int4:    return GL_INT;
+    case ShaderDataType::Mat3:    return GL_FLOAT;
+    case ShaderDataType::Mat4:    return GL_FLOAT;
+    }
+
+    BS_ASSERT(false, "Unknown ShaderDataType specified")
+      return 0;
+  }
+
   Application::Application()
   {
     BS_ASSERT(!s_Instance, "Application already exists!");
@@ -33,33 +55,58 @@ namespace Buckshot {
 
     float vertices[] =
     {
-      -0.5f, -0.5f, 0.0f,
-       0.5f, -0.5f, 0.0f,
-       0.0f,  0.5f, 0.0f
+      -0.5f, -0.5f, 0.0f, 0.1f, 0.2f, 0.3f, 1.0f,
+       0.5f, -0.5f, 0.0f, 0.4f, 0.5f, 0.6f, 1.0f,
+       0.0f,  0.5f, 0.0f, 0.7f, 0.8f, 0.9f, 1.0f
     };
-
-    m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
-    m_VertexBuffer->Bind();
-
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, nullptr);
 
     uint32_t indices[] =
     {
       0, 1, 2
     };
 
+
+    m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
+    m_VertexBuffer->Bind();
+
+    {
+      BufferLayout layout = {
+            { ShaderDataType::Float3, "a_Position" },
+            { ShaderDataType::Float4, "a_Color" }
+      };
+      m_VertexBuffer->SetLayout(layout);
+    }
+
+    uint32_t index = 0;
+    const auto& layout = m_VertexBuffer->GetLayout();
+    for (const auto& element : layout)
+    {
+      glEnableVertexAttribArray(index);
+      glVertexAttribPointer(
+        index, 
+        element.GetComponentCount(), 
+        ShaderDataTypeToOpenGLBaseType(element.DataType), 
+        (element.Normalized) ? GL_FALSE : GL_TRUE, 
+        layout.GetStride(),
+        (const void*)element.Offset);
+      index++;
+    }
+
+
     m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
 
     std::string vertexSrc = R"(
     #version 330 core
-    layout (location = 0) in vec3 a_Position;  
+    layout (location = 0) in vec3 a_Position;   
+    layout (location = 1) in vec4 a_Color;     
     
     out vec3 v_Position;
+    out vec4 v_Color;
     
     void main()
     {
       v_Position = a_Position +0.5;
+      v_Color = a_Color;
       gl_Position = vec4(a_Position + 0.5, 1.0f); 
     }
     )";
@@ -68,11 +115,13 @@ namespace Buckshot {
     #version 330 core
 
     in vec3 v_Position;
+    in vec4 v_Color;
+
     out vec4 a_Color;  
     
     void main()
     {
-      a_Color = vec4(v_Position * 0.5 + 0.5, 1.0);
+      a_Color = v_Color;
     }
     )";
 
